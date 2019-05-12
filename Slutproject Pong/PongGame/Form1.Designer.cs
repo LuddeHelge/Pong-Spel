e9b4478736a3488383815a5c49c472743c54bec4
LuddeHelge <46518181+LuddeHelge@users.noreply.github.com>
2019-05-12 18:42:52 +0200

    Nya pongspelet
    Gjorde dethär eftersom jag låg inne på sjukhus i 3 veckor och känner att jag inte har tid att lära mig monogame. Det är ett simpelt pong spel i forms och tror det ska vara nog för ett E vilket är helt ok med tanke på att jag siktar på E - C i slutbetyg

---
+++ Slutproject Pong/PongGame/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PongSpel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Paddle1 = new System.Windows.Forms.PictureBox();
+            this.Paddle2 = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Paddle1
+            // 
+            this.Paddle1.BackColor = System.Drawing.SystemColors.Control;
+            this.Paddle1.Location = new System.Drawing.Point(12, 180);
+            this.Paddle1.Name = "Paddle1";
+            this.Paddle1.Size = new System.Drawing.Size(22, 119);
+            this.Paddle1.TabIndex = 0;
+            this.Paddle1.TabStop = false;
+            // 
+            // Paddle2
+            // 
+            this.Paddle2.BackColor = System.Drawing.SystemColors.Control;
+            this.Paddle2.Location = new System.Drawing.Point(955, 180);
+            this.Paddle2.Name = "Paddle2";
+            this.Paddle2.Size = new System.Drawing.Size(22, 119);
+            this.Paddle2.TabIndex = 1;
+            this.Paddle2.TabStop = false;
+            // 
+            // Boll
+            // 
+            this.Ball.BackColor = System.Drawing.SystemColors.Control;
+            this.Ball.Location = new System.Drawing.Point(470, 282);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(15, 17);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // Tidtagaren
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(461, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(493, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 51);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "|";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleName = "RightScore";
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(530, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 51);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleName = "LeftScore";
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(444, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 51);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(989, 539);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Paddle2);
+            this.Controls.Add(this.Paddle1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paddle2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Paddle1;
+        private System.Windows.Forms.PictureBox Paddle2;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Slutproject Pong/PongGame/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongSpel
+{
+    public partial class Form1 : Form
+    {
+        bool upPressed, downPressed;
+        bool OPupPressed, OPdownPressed;
+        const int Movementspeed = 4;
+        int BallSpeedX;
+        int BallSpeedY;
+        int PlayerScore = 0;
+        int AiScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetBall();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //-- Paddelens Kontroll
+            if(upPressed) {
+                Paddle2.Location = new Point(Paddle2.Location.X, Math.Max(0,Paddle2.Location.Y - Movementspeed));
+            } else if (downPressed) {
+                Paddle2.Location = new Point(Paddle2.Location.X, Math.Min(425, Paddle2.Location.Y + Movementspeed));
+            }
+
+
+            if (OPupPressed) {
+                Paddle1.Location = new Point(Paddle1.Location.X, Math.Max(0,Paddle1.Location.Y - Movementspeed));
+            } else if (OPdownPressed) {
+                Paddle1.Location = new Point(Paddle1.Location.X, Math.Min(425, Paddle1.Location.Y + Movementspeed));
+            }
+
+
+            //-- Bollens Kontroll
+            if(Ball.Location.Y > 525 || Ball.Location.Y < -2)  { BallSpeedY = BallSpeedY * -1; };
+            if (Paddle1.Bounds.Contains(Ball.Bounds) || Paddle2.Bounds.Contains(Ball.Bounds)) { BallSpeedX = BallSpeedX * -1; };
+            Ball.Location = new Point(Ball.Location.X + BallSpeedX, Ball.Location.Y + BallSpeedY);
+            if (Ball.Location.X > 976 ) { 
+                ResetBall();
+                PlayerScore++;
+                label4.Text = PlayerScore.ToString();
+            }; 
+            if (Ball.Location.X < 0) { // Spelaren
+                ResetBall();
+                AiScore++;
+                label3.Text = AiScore.ToString();
+
+            }; 
+
+            //-- Datorns kontroll
+            if (Ball.Location.X > 300) {
+                if (Paddle2.Location.Y > Ball.Location.Y) { upPressed = true; } else { upPressed = false; };
+                if (Paddle2.Location.Y < Ball.Location.Y) { downPressed = true; } else { downPressed = false; };
+            }
+          
+
+        }
+
+        //-- Knappar
+        private void Form1_KeyDown(object sender, KeyEventArgs e) {
+            
+            if (e.KeyCode == Keys.W) { OPupPressed = true; } else if (e.KeyCode == Keys.S) { OPdownPressed = true; }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e) {
+            
+            if (e.KeyCode == Keys.W) { OPupPressed = false; } else if (e.KeyCode == Keys.S) { OPdownPressed = false; }
+        }
+
+        public void ResetBall() {
+            Thread.Sleep(1000);
+            Random random = new System.Random();
+            Ball.Location = new Point(476,240);
+            BallSpeedY = random.Next(-10, 10);
+            BallSpeedX = random.Next(-8, 8);
+            
+        }
+
+
+    }
+}
